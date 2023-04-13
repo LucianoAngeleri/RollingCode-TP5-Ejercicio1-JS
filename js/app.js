51086e49c4a5a92ea67f18df8166dbac156a3006
Luciano Angeleri <lcnang@gmail.com>
2023-04-13 16:09:58 -0300

    Se agregó el form

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let btnInicio = document.getElementById("#btnInicio")
+console.log(btnInicio)
+btnInicio.addEventListener("click",btnInicio)
+let inputNumero = document.getElementsByName("inputNumero")
+
+
+
+function mostrarForm(){
+    console.log("Desde mostrarForm")
+}
