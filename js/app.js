926e5e45f5f591db66c1404b55cb2e19ce0b5aac
Luciano Angeleri <lcnang@gmail.com>
2023-04-13 16:47:59 -0300

    Se agregó botones

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-let btnInicio = document.getElementById("#btnInicio")
+let btnInicio = document.getElementById("btnInicio")
 console.log(btnInicio)
-btnInicio.addEventListener("click",btnInicio)
+btnInicio.addEventListener("click",mostrarForm)
 let inputNumero = document.getElementsByName("inputNumero")
 
-
+let numeroAleatorio = 0;
 
 function mostrarForm(){
     console.log("Desde mostrarForm")
+    numeroAleatorio = Math.floor(Math.random() * 10)
+    btnInicio.innerHTML = "Volver a comenzar"
+    btnInicio.className = "btn btn-danger fs-3"
 }
