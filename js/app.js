7df04a8d2a747de0757c8676eecf4608c4a8b872
Luciano Angeleri <lcnang@gmail.com>
2023-04-14 04:18:59 -0300

    Se creó la lógica para generar el número y decir si es mayor o menor

--- js/app.js
+++ js/app.js
@@ -1,15 +1,34 @@
 let btnInicio = document.getElementById("btnInicio")
 let contenedorJuego = document.getElementById("contenedorJuego")
-btnInicio.addEventListener("click",mostrarForm)
-let inputNumero = document.getElementsByName("inputNumero")
+let formulario = document.querySelector("form")
+let numeroGenerado = 0;
 
-let numeroAleatorio = 0;
+btnInicio.addEventListener("click",inicioJuego)
+formulario.addEventListener("submit",verGanador)
 
-function mostrarForm(){
-    console.log("Desde mostrarForm")
-    numeroAleatorio = Math.floor(Math.random() * 10)
-    btnInicio.innerHTML = "Volver a comenzar"
-    btnInicio.className = "btn btn-danger fs-3"
-    contenedorJuego.className = "visible"
 
+function generarNumero(){
+    return Math.floor(Math.random() * 10)+1
 }
+
+function inicioJuego(){
+    numeroGenerado = generarNumero()
+    console.log(numeroGenerado)
+    console.log(typeof(numeroGenerado))
+    contenedorJuego.className = "visible"
+    btnInicio.className = "btn btn-danger fs-3"
+    btnInicio.innerHTML = "Volver a comenzar"
+}
+function verGanador(e){
+    e.preventDefault();
+    let inputNumero = document.getElementById("inputNumero")
+    if (inputNumero.value == numeroGenerado) {
+        console.log("Adivinaste el número")
+    }else if(inputNumero.value >= numeroGenerado){
+        console.log("El número que elegiste es mayor")
+    }else{
+        console.log("El número que elegiste es menor")
+    }
+    formulario.reset();
+}
+
