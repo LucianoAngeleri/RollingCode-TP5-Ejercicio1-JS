ac436cdecccf09162c80454490cabd9d105537f1
Luciano Angeleri <lcnang@gmail.com>
2023-04-15 01:15:31 -0300

    Se agregó la validacion del input, cambios en el UI, se agregó alerta

--- js/app.js
+++ js/app.js
@@ -3,32 +3,52 @@ let contenedorJuego = document.getElementById("contenedorJuego")
 let formulario = document.querySelector("form")
 let numeroGenerado = 0;
 
-btnInicio.addEventListener("click",inicioJuego)
-formulario.addEventListener("submit",verGanador)
+btnInicio.addEventListener("click", inicioJuego)
+formulario.addEventListener("submit", verResultado)
 
 
-function generarNumero(){
-    return Math.floor(Math.random() * 10)+1
+function generarNumero() {
+    return Math.floor(Math.random() * 10) + 1
 }
 
-function inicioJuego(){
+function inicioJuego() {
     numeroGenerado = generarNumero()
     console.log(numeroGenerado)
-    console.log(typeof(numeroGenerado))
-    contenedorJuego.className = "visible"
-    btnInicio.className = "btn btn-danger fs-3"
-    btnInicio.innerHTML = "Volver a comenzar"
+    contenedorJuego.className = "d-block"
+    btnInicio.className = "d-none"
+    // btnInicio.className = "btn btn-danger fs-3"
+    // btnInicio.innerHTML = "Volver a comenzar"
 }
-function verGanador(e){
+function verResultado(e) {
     e.preventDefault();
     let inputNumero = document.getElementById("inputNumero")
+    let contenedorAlertResultado = document.getElementById("contenedorAlertResultado")
+    let alertResultado = document.getElementById("alertResultado")
+    console.log(alertResultado)
+    let condicion = true;
     if (inputNumero.value == numeroGenerado) {
-        console.log("Adivinaste el número")
-    }else if(inputNumero.value >= numeroGenerado){
-        console.log("El número que elegiste es mayor")
-    }else{
-        console.log("El número que elegiste es menor")
+        contenedorAlertResultado.className = "container visible"
+        alertResultado.className = "alert alert-success"
+        alertResultado.innerHTML = "Elegiste el número correcto!"
+        condicion = true
+    } else if (inputNumero.value > numeroGenerado) {
+        contenedorAlertResultado.className = "container visible"
+        alertResultado.className = "alert alert-secondary"
+        alertResultado.innerHTML = "El número ingresado es mayor, <strong>elige un número mas bajo</strong>"
+        condicion = false
+    } else {
+        contenedorAlertResultado.className = "container visible"
+        alertResultado.className = "alert alert-secondary"
+        alertResultado.innerHTML = "El número ingresado es menor, <strong>elige un número mas alto</strong>"
+        condicion = false
+    }
+    if (condicion === true) {
+        contenedorJuego.className = "d-none"
+        btnInicio.className = "d-block"
+        btnInicio.className = "btn btn-danger fs-3"
+        btnInicio.innerHTML = "Volver a jugar"
     }
     formulario.reset();
+
 }
 
