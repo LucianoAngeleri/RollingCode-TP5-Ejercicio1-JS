92924903e7303a304e158eb1fdaeb772da5c09f2
Luciano Angeleri <lcnang@gmail.com>
2023-04-15 03:24:19 -0300

    Se modifico los botones y el navbar

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function inicioJuego() {
     console.log(numeroGenerado)
     contenedorAlertResultado.className = "d-none"
     contenedorJuego.className = "d-block"
-    formulario.className = "text-center d-flex flex-column gap-3"
+    formulario.className = "text-center d-flex flex-column gap-3 px-md-5"
     btnInicio.className = "d-none"
 }
 function verResultado(e) {
@@ -26,25 +26,25 @@ function verResultado(e) {
     let alertResultado = document.getElementById("alertResultado")
     condicion = false;
     if (inputNumero.value == numeroGenerado) {
-        contenedorAlertResultado.className = "d-block"
-        alertResultado.className = "alert alert-success"
+        contenedorAlertResultado.className = "d-block px-md-5"
+        alertResultado.className = "alert alert-success fs-5"
         alertResultado.innerHTML = "Elegiste el número correcto!"
         condicion = true; 
     } else if (inputNumero.value > numeroGenerado) {
-        contenedorAlertResultado.className = "container visible"
-        alertResultado.className = "alert alert-secondary"
+        contenedorAlertResultado.className = "d-block px-md-5"
+        alertResultado.className = "alert alert-secondary fs-5"
         alertResultado.innerHTML = "El número ingresado es mayor, <strong>elige un número mas bajo</strong>"
         condicion = false;
     } else {
-        contenedorAlertResultado.className = "container visible"
-        alertResultado.className = "alert alert-secondary"
+        contenedorAlertResultado.className = "d-block px-md-5"
+        alertResultado.className = "alert alert-secondary fs-5"
         alertResultado.innerHTML = "El número ingresado es menor, <strong>elige un número mas alto</strong>"
         condicion = false;
     }
     if (condicion === true) {
         formulario.className = "d-none"
         btnInicio.className = "d-block"
-        btnInicio.className = "btn btn-danger fs-3"
+        btnInicio.className = "btn btn-outline-success tn-lg rounded-pill p-4 w-75 align-self-center fs-3"
         btnInicio.innerHTML = "Volver a jugar"
     }
     formulario.reset();
